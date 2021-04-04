cdada99d2e668638ba5c90595cb26a626fc85970
max27 <max1>
2021-04-04 10:13:08 +0200

    Ajout page Mycars.js et Cars.js

---
+++ src/components/Mycars.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Mycars extends Component{
+    render() {
+        return (
+            <div>
+                <h1>Page My cars</h1>
+            </div>
+        )
+    }
+}
+
+/*
+** Pour pouvoir l'importer dans une autre classe 
+ */
+export default Mycars;
